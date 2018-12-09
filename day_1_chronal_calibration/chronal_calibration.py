eaae61b4d19a260fed54d19b28c08194a8660334
Cole Canning <ccanning10@gmail.com>
2018-12-08 19:11:19 -0500

    finished d 1 p 2

--- day_1_chronal_calibration/chronal_calibration.py
+++ day_1_chronal_calibration/chronal_calibration.py
@@ -1,18 +1,23 @@
 from input import raw_values
 
-# raw_values = '''
-# +1
-# -50
-# +1
-# '''
 
 raw_values = [val for val in raw_values.split('\n') if val]
-
 final_frequency = 0
-for input_value in raw_values:
-    value = int(input_value[1:])
-    if '+' in input_value:
-        final_frequency += value
-    if '-' in input_value:
-        final_frequency -= value
+frequencies = set()
+done = False
+
+while not done:
+    for input_value in raw_values:
+        value = int(input_value[1:])
+        if '+' in input_value:
+            final_frequency += value
+        if '-' in input_value:
+            final_frequency -= value
+        if final_frequency in frequencies:
+            done = True
+            break
+        else:
+            frequencies.add(final_frequency)
+
+print(len(frequencies))
 print(final_frequency)
